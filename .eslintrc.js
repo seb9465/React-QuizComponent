c431ce4ace91324a2a8af30fc9314082ea982803
Sébastien Cadorette <cado.94@me.com>
2018-06-04 14:14:06 -0400

    Module 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
-module.exports = {
-    "extends": "airbnb"
+export default {
+    "extends": ["eslint:recommended", "plugin:react/recommended", "airbnb"],
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "mocha": true,
+        "node": false
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "constructor-super": "warn",
+        "valid-typeof": "warn"
+    }
 };
