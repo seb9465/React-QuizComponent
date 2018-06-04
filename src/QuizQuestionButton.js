ce869156b22d438674f9394f25f180df68a02c16
Sébastien Cadorette <cado.94@me.com>
2018-06-04 14:28:28 -0400

    Module 3
    Adding QuizQuestionButton class.

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class QuizQuestionButton extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { }
+    }
+
+    render () {
+        return (
+            <div />
+        );
+    }
+}
+
+export default QuizQuestionButton;
