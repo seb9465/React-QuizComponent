e02058dd93d5bbb8d237c66ddf1c2a2d262b96a2
Sébastien Cadorette <cado.94@me.com>
2018-06-04 15:52:30 -0400

    Module 6
    set state properly

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,8 +13,12 @@ class QuizQuestion extends Component {
     }
 
     handleClick (buttonText) {
-        if (buttonText === this.props.quiz_question.answer)
+        if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+            this.setState( () => { return {incorrectAnswer: false}});
+        } else {
+            this.setState( () => { return {incorrectAnswer: true}});
+        }
     }
 
     render() {
